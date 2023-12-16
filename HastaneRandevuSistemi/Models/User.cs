b7d8f623136d76cdc365a21f9a1e782fa70d648a
tberkk <93779700+tberkk@users.noreply.github.com>
2023-12-16 13:23:54 +0300

    login

---
+++ HastaneRandevuSistemi/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HastaneRandevuSistemi.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserID { get; set; }
+
+        [StringLength(20)]
+        public string KullaniciAdi { get; set; }
+
+        [StringLength(20)]
+        public string Sifre { get; set; }
+
+        [StringLength(1)]
+        public string Rol { get; set; }
+    }
+}
--- HastaneRandevuSistemi/Program.cs
+++ HastaneRandevuSistemi/Program.cs
@@ -1,10 +1,17 @@
 using HastaneRandevuSistemi.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+/*builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("AppDbContextConnection")));*/
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<AppDbContext>();
 
 var app = builder.Build();
 
@@ -20,6 +27,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();;
 
 app.UseAuthorization();
 
